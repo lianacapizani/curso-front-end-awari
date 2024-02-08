f0df95c6535bf607600f0f29d78d48404461e1f8
Liana Karla Capizani <lianacapizani@gmail.com>
2024-02-08 16:07:00 -0300

    insert aula 3

---
+++ aula3/scripts.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = form.elements["name"].value;
+  const email = form.elements["email"].value;
+  const message = form.elements["message"].value;
+  const assunto = form.assunto["message"].value;
+
+  const result = `Nome: ${name}\nE-mail: ${email}\nAssunto: ${assunto}\nMessage: ${message}`;
+  alert(result);
+});
